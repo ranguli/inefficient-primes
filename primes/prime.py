cd62c5ee7956b7bc23873621db54b6bc8be5d308
Joshua Murphy <joshuamurphy71@gmail.com>
2018-09-29 18:59:11 -0230

    General improvements

--- primes/prime.py
+++ primes/prime.py
@@ -1,62 +1,70 @@
-import time
+import time, csv, sys
+assert sys.version_info >= (3,0)
+
 import matplotlib.pyplot as plt
-import csv
 
 
-outputFile = "primes.csv"
+
+datafile = "primes.csv"
 
 x = []
 y = []
 
+
 def isPrime(i):
-    prime = True 
+    prime = True
     for j in range(2, i):
         if (i % j == 0):
             prime = False
-    return prime 
+    return prime
 
-def generateAllPrimes(i, outputFile):
+
+def generateAllPrimes(i, datafile):
     startTime = time.time()
-    f = open(filename, "w")
+    f = open(datafile, "w")
     for j in range(i):
         if isPrime(j):
-            executionTime = round(time.time() - startTime, 3)
-            f.write(str(j) + "," + str(executionTime) + "\n") 
-    f.close() 
+            executionTime = int(time.time() - startTime)
+            f.write(str(j) + "," + str(executionTime) + "\n")
+    f.close()
 
-def main():
-    i = int(input("Enter a number to generate primes below it:"))
-    startTime = time.time() 
-    primes = generateAllPrimes(i)
+
+def main(datafile):
+    i = int(input("Enter a number to generate primes below it: "))
+    startTime = time.time()
+    primes = generateAllPrimes(i, datafile)
     if (isPrime(i)):
-        print(str(i) + " itself is a prime number") 
+        print(str(i) + " itself is a prime number,", end='')
     else:
-        print(str(i) + " itself is not a prime number.")
+        print(str(i) + " itself is not a prime number,", end='')
 
-    print("It took " + str(round(time.time() - startTime, 2) ) + " seconds to generate primes below it.")
-    print("The results have been save to \"primes.txt\"")
+    print(" and it took " + str(round(time.time() - startTime, 2)) +
+          " seconds to figure that out.")
+    print("*The results have been save to " + str(datafile) + "*" )
+    print("Your number is " + str(23249425 - i) + " less than the highest prime number ever discovered.")
 
-def plotTime(inputFile):
-   
+
+def plotTime(datafile):
+
     # Frankly, I didn't care to learn matplotlib this time around.
-    # This code is from: 
+    # This code is from:
     # https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
 
     x = []
     y = []
-  
-    with open(inputfile, 'r') as csvfile:
-        plots = csv.reader(csvfile,delimiter=',')
+
+    with open(datafile, 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
-        x.append(int[rows[0]))
-        y.append(int[rows[1]))
-    plt.plot(x,y label='Loaded from file!')
+            x.append(int(row[0]))
+            y.append(int(row[1]))
+    plt.plot(x, y, label = 'Iterative Generation')
     plt.xlabel('Prime Number')
-    plt.ylaber('Execution Time')
-    plt.tiel("Prime Number Generation over Execution Time")
+    plt.ylabel('Execution Time')
+    plt.title("Prime Number Generation over Execution Time")
     plt.legend()
     plt.show()
 
-main() 
-plotTime()
 
+main(datafile)
+plotTime(datafile)
