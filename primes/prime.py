c268659d03475ea8e2be1154711395006cb13bab
ranguli <joshuamurphy71@gmail.com>
2018-09-30 11:01:29 -0230

    Abstracted methods into classes

--- primes/prime.py
+++ primes/prime.py
@@ -1,69 +1,54 @@
 import time, csv, sys
+from numexpr import cpuinfo
+
+from algorithms import *
 
 assert sys.version_info >= (3,0)
-
-from numexpr import cpuinfo
 import matplotlib.pyplot as plt
 
 
-
-datafile = "primes.csv"
-
-x = []
-y = []
-
-def isPrime(i):
-    prime = True
-    for j in range(2, i):
-        if (i % j == 0):
-            prime = False
-    return prime
-
-def generateAllPrimes(i, datafile):
-    startTime = time.time()
-    f = open(datafile, "w")
-    for j in range(i):
-        if isPrime(j):
-            executionTime = int(time.time() - startTime)
-            f.write(str(j) + "," + str(executionTime) + "\n")
-    f.close()
-
-def main(datafile):
+def main():
+  
     i = int(input("Enter a number to generate primes below it: "))
-    startTime = time.time()
-    primes = generateAllPrimes(i, datafile)
-    if (isPrime(i)):
-        print(str(i) + " itself is a prime number,", end = '')
-    else:
-        print(str(i) + " itself is not a prime number,", end = '')
+    algorithm1 = algorithms.iterative1(i, "algorithm1_results.csv")
+    algorithm1.generateAllPrimes(i, "algorithm1_results.csv")
 
-    print(" and it took " + str(round(time.time() - startTime, 2)) +
-          " seconds to figure that out.")
-    print("*The results have been save to " + str(datafile) + "*" )
-    print("Your number is " + str(23249425 - i) + " less than the highest prime number ever discovered.")
+    algorithm2 = algorithms.iterative2(i, "algorithm2_results.csv")
+    algorithm2.generateAllPrimes(i, "algorithm2_results.csv")
+    
+    Plot.plot("algorithm1_results.csv")
+    Plot.plot("algorithm2_results.csv")
+    Plot.graph()        
+
+class Plot:
+
+    def plot(datafile): 
+
+        # Frankly, I didn't care to learn matplotlib this time around.
+        # This code is from:
+        # https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
+  
+        x = []
+        y = []
+        cpu = cpuinfo.cpu.info[0]['model name']
+
+        with open(datafile, 'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            for row in plots:
+                x.append(int(row[0]))
+                y.append(int(row[1]))
+    
+        plt.plot(x, y, label = "algorithm1(" + cpu + ")")
+
+    def graph():
+        plt.xlabel('Prime Number')
+        plt.ylabel('Execution Time')
+        plt.title("Prime Number Generation over Execution Time")
+        plt.legend()
+        plt.show()
+
+main()
 
 
-def plotTime(datafile): 
-    # Frankly, I didn't care to learn matplotlib this time around.
-    # This code is from:
-    # https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
-
-    x = []
-    y = []
-    cpu = cpuinfo.cpu.info[0]['model name']
-
-    with open(datafile, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        for row in plots:
-            x.append(int(row[0]))
-            y.append(int(row[1]))
-    plt.plot(x, y, label = "Iterative (" + cpu + ")")
-    plt.xlabel('Prime Number')
-    plt.ylabel('Execution Time')
-    plt.title("Prime Number Generation over Execution Time")
-    plt.legend()
-    plt.show()
 
 
-main(datafile)
-plotTime(datafile)
